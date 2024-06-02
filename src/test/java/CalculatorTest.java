ca5fa40f9c4e1779f4ee9e651a72202604a31384
Ryunehaimu <ryunehaimu007@gmail.com>
2024-06-02 14:10:51 +0700

    coverage code masih dibawah 80%

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,29 +14,29 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        assertEquals(15, calculator.add(10, 5), 0);
-        assertEquals(0, calculator.add(-2, 2), 0);
-        assertEquals(-3, calculator.add(-1, -2), 0);
+        assertEquals("10 + 5 should equal 15", 15, calculator.add(10, 5), 0);
+        assertEquals("-2 + 2 should equal 0", 0, calculator.add(-2, 2), 0);
+        assertEquals("-1 + -2 should equal -3", -3, calculator.add(-1, -2), 0);
     }
 
     @Test
     public void testSubtract() {
-        assertEquals(5, calculator.subtract(10, 5), 0);
-        assertEquals(-4, calculator.subtract(-2, 2), 0);
-        assertEquals(1, calculator.subtract(-1, -2), 0);
+        assertEquals("10 - 5 should equal 5", 5, calculator.subtract(10, 5), 0);
+        assertEquals("-2 - 2 should equal -4", -4, calculator.subtract(-2, 2), 0);
+        assertEquals("-1 - -2 should equal 1", 1, calculator.subtract(-1, -2), 0);
     }
 
     @Test
     public void testMultiply() {
-        assertEquals(50, calculator.multiply(10, 5), 0);
-        assertEquals(-4, calculator.multiply(-2, 2), 0);
-        assertEquals(2, calculator.multiply(-1, -2), 0);
+        assertEquals("10 * 5 should equal 50", 50, calculator.multiply(10, 5), 0);
+        assertEquals("-2 * 2 should equal -4", -4, calculator.multiply(-2, 2), 0);
+        assertEquals("-1 * -2 should equal 2", 2, calculator.multiply(-1, -2), 0);
     }
 
     @Test
     public void testDivide() {
-        assertEquals(2, calculator.divide(10, 5), 0);
-        assertEquals(-1, calculator.divide(-2, 2), 0);
-        assertEquals(0.5, calculator.divide(-1, -2), 0);
+        assertEquals("10 / 5 should equal 2", 2, calculator.divide(10, 5), 0);
+        assertEquals("-2 / 2 should equal -1", -1, calculator.divide(-2, 2), 0);
+        assertEquals("-1 / -2 should equal 0.5", 0.5, calculator.divide(-1, -2), 0);
     }
 }
