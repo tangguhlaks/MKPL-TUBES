66b2341725930330cd21e4bd51ae748229de9ed8
Ryunehaimu <ryunehaimu007@gmail.com>
2024-06-02 14:12:59 +0700

    remove main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,5 @@ import org.example.code.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println("2 + 3 = " + calculator.add(2, 3));
-        System.out.println("3 - 2 = " + calculator.subtract(3, 2));
-        System.out.println("2 * 3 = " + calculator.multiply(2, 3));
-        System.out.println("4 / 2 = " + calculator.divide(4, 2));
     }
 }
